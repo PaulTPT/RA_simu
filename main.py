5d685199e49e66495924b3436851aa5b1ecfc18d
unknown <paul.berthier@outlook.com>
2015-08-20 14:29:16 -0400

    Clean code and add comments

--- main.py
+++ main.py
@@ -4,29 +4,45 @@ import time
 import sys
 
 PROCESSES_NB = None # The number of processes will be the number of cores available on the system
-TASKS_NBR = 10000 #Number of tasks to allocate to the cores
+TASKS_NBR = 1000 # Number of tasks to allocate to the cores
 TASK_CPTIME_MIN =1 # Minimum time to compute a task 
 TASK_CPTIME_MAX =100 # Miaximum time to compute a task 
-NUM_CPU = cpu_count() #The number of CPUs available on the machine
+NUM_CPU = cpu_count() # The number of CPUs available on the machine
 
 random.seed() # Generates a seed for the random generator
 
+# Generator generating n tasks with a random execution time
 def tasks_generator(nbr):
 	for i in xrange(nbr):
 		yield {'name':"Task" + str(i+1), 'duration':random.randint(TASK_CPTIME_MIN,TASK_CPTIME_MAX)}
 
+# The code running on each core
 def tasks_handler(queue,process_nb,lock):
 	while not queue.empty():
-		task=queue.get()
-		# lock.acquire()
-		# print 'Executing task ' + task['name'] + ' on process ' + str(process_nb)
-		# lock.release()
-		time.sleep(task['duration']/1000)
+		task=queue.get() # We get on the queue one of the tasks allocated by the tasks allocator 
+		lock.acquire()
+		print 'Executing task ' + task['name'] + ' on process ' + str(process_nb)
+		lock.release()
+		time.sleep(task['duration']/1000) # Simulation of the execution time
+
+# The function allocating tasks to the queues of the different cores
+def tasks_allocator(tasks,queues):
+		# Implement your ressource allocation algorithm here
+		#
+		# Exemple: Random allocation
+		num_process=len(queues)-1
+	   	for task in tasks:
+	   		pr_num=random.randint(0,num_process-1)
+	   		queues[pr_num].put(task)
+
 
 if __name__ == '__main__':
+
 	lock=Lock()
    	process_list=[]
    	queues_list=[]
+
+   	# Initialisation of the processes and their queues
    	for i in range(0,NUM_CPU) :
    		queue=Queue()
    		queues_list.append(queue)
@@ -35,9 +51,8 @@ if __name__ == '__main__':
 
    	startTime=time.time()
 
-   	for task in tasks_generator(TASKS_NBR):
-   		pr_num=random.randint(0,NUM_CPU-1)
-   		queues_list[pr_num].put(task)
+   	tasks=tasks_generator(TASKS_NBR)
+   	tasks_allocator(tasks,queues_list)
 
    	for process in process_list:
    		process.start()
@@ -46,6 +61,8 @@ if __name__ == '__main__':
 		process.join()
 
    	endTime=time.time()
+
 	#calculate the total time it took to complete the work
 	workTime =  endTime - startTime
+
 	print "The jobs took " + str(workTime) + " seconds to complete"
