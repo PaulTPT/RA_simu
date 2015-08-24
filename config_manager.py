6b3fa292b1c5e91fdb13e63207db4e306e5fc4c2
unknown <paul.berthier@outlook.com>
2015-08-24 16:11:57 -0400

    Ajout du chargement d'une config existante

--- config_manager.py
+++ config_manager.py
@@ -1,5 +1,3 @@
-# import numpy
-import random
 from os import path
 from os import mkdir
 
@@ -8,8 +6,6 @@ TASK_CPTIME_MIN =1 # Minimum time to compute a task
 TASK_CPTIME_MAX =100 # Miaximum time to compute a task
 DELAY_MIN =1 # Minimum delay
 DELAY_MAX =10 # Miaximum delay
-
-random.seed() # Generates a seed for the random generator
 
 def generate_config (config_name, NUM_CPU):
 	config_path="configs/"+str(config_name)
@@ -42,11 +38,13 @@ def generate_config (config_name, NUM_CPU):
 
 
 
-# Generator generating n tasks with a random execution time
+# Generator generating nbr tasks with a random execution time
 def tasks_generator(nbr):
 	for i in xrange(nbr):
-		yield {'name':"Task" + str(i+1), 'duration':random.randint(TASK_CPTIME_MIN,TASK_CPTIME_MAX)}
+		yield {'name':"Task" + str(i+1), 'duration': random.randint(TASK_CPTIME_MIN,TASK_CPTIME_MAX)}
 
+
+# Generator generating the communication delays between the different CPU (blades)
 def net_delay_generator(nbr_cpu):
 	for i in xrange(nbr_cpu):
 		delays=[]
@@ -55,4 +53,31 @@ def net_delay_generator(nbr_cpu):
 		delays[i]=0
 		yield delays
 	
+
+def load_tasks(config_name,NUM_CPU):
+	config_path="configs/"+str(config_name)
+	file_path_tasks= path.relpath(config_path+"/tasks.cfg")
+
+	try:
+		tasks_data=open(file_path_tasks,'r')
+	except IOError:
+		print ('Config does not exists. Generating it...'+'\n')	
+		generate_config(config_name,NUM_CPU)
+		return load_tasks_config(config_name,NUM_CPU)
+		
+	return parse_tasks_config(tasks_data)
+
+def parse_tasks_config(tasks_data):
+	try:
+		for line in tasks_data:
+			line_words=line.split()
+			yield {'name': line_words[0], 'duration': int(line_words[1])}
+	finally:
+		tasks_data.close()
+
+
+
+
+
+
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from multiprocessing import cpu_count, Queue, Process, Lock
+import random
 import time
 import sys
-# import numpy
 import config_manager
+
+random.seed() # Generates a seed for the random generator
 
 PROCESSES_NB = None # The number of processes will be the number of cores available on the system
 NUM_CPU = cpu_count() # The number of CPUs available on the machine
@@ -12,9 +14,9 @@ def tasks_handler(queue,process_nb,lock):
 	while not queue.empty():
 		task=queue.get() # We get on the queue one of the tasks allocated by the tasks allocator 
 		lock.acquire()
-		print 'Executing task ' + task['name'] + ' on process ' + str(process_nb)
+		# print 'Executing task ' + task['name'] + ' on process ' + str(process_nb)
 		lock.release()
-		time.sleep(task['duration']/1000) # Simulation of the execution time
+		time.sleep(task['duration']/1000.0) # Simulation of the execution time
 
 # The function allocating tasks to the queues of the different cores
 def tasks_allocator(tasks,queues):
@@ -32,10 +34,11 @@ if __name__ == '__main__':
 	lock=Lock()
    	process_list=[]
    	queues_list=[]
+   	config_name=""
 
 
    	if len(sys.argv)==2:
-   		pass
+   		config_name=sys.argv[1]
 	elif len(sys.argv)==1:
 		config_name=raw_input("Give a name to the new config:\n")
 		config_manager.generate_config(config_name, NUM_CPU)
@@ -43,27 +46,29 @@ if __name__ == '__main__':
 		print "Please enter the folder containing the config you weant to use or no argument if you want to generate a new one."
 		sys.exit()
 
- #   	# Initialisation of the processes and their queues
- #   	for i in range(0,NUM_CPU) :
- #   		queue=Queue()
- #   		queues_list.append(queue)
- #   		process=Process(target=tasks_handler,args=(queue,i,lock))
- #   		process_list.append(process)
 
- #   	startTime=time.time()
+   	# Initialisation of the processes and their queues
+   	for i in range(0,NUM_CPU) :
+   		queue=Queue()
+   		queues_list.append(queue)
+   		process=Process(target=tasks_handler,args=(queue,i,lock))
+   		process_list.append(process)
 
- #   	tasks=tasks_generator(TASKS_NBR)
- #   	tasks_allocator(tasks,queues_list)
+   	startTime=time.time()
 
- #   	for process in process_list:
- #   		process.start()
+   	# tasks=tasks_generator(TASKS_NBR)
+   	tasks=config_manager.load_tasks(config_name,NUM_CPU)
+   	tasks_allocator(tasks,queues_list)
 
-	# for process in process_list:
-	# 	process.join()
+   	for process in process_list:
+   		process.start()
 
- #   	endTime=time.time()
+	for process in process_list:
+		process.join()
 
-	# #calculate the total time it took to complete the work
-	# workTime =  endTime - startTime
+   	endTime=time.time()
 
-	# print "The jobs took " + str(workTime) + " seconds to complete"
+	#calculate the total time it took to complete the work
+	workTime =  endTime - startTime
+
+	print "The jobs took " + str(workTime) + " seconds to complete"
